32780a42414c8df0694ecbf1ac94868e272a9701
Kevin Feng <kevinfbths@gmail.com>
2023-11-24 13:38:19 -0500

    initial setup

---
+++ my-app/src/components/ViewListings.jsx
@@ -0,0 +1,11 @@
+import React from 'react'; 
+
+const ViewListings = () => {
+  return ( 
+    <div>
+        Default page, although not sure how the info should be displayed yet, will work on it 
+    </div>
+  )
+}
+
+export default ViewListings;
