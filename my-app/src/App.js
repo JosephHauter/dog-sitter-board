a930b1d0b236bd46071a1054901d506ac35287f9
unknown <tk.anie123@gmail.com>
2023-11-24 19:35:09 -0500

    Started homepage, finished header, cleaned up css of about-me page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,54 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import Header from "./components/Header.jsx";
 import './App.css';
 
 function App() {
-  const [name, setName] = useState('');
-  const [about, setAbout] = useState('');
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    fetch('http://localhost:5000/users')
-      .then(response => response.json())
-      .then(data => setUsers(data))
-      .catch(error => console.error('Error:', error));
-  }, []);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    fetch('http://localhost:5000/users', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ name, about })
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      setUsers(prevUsers => [...prevUsers, data]);
-    })
-    .catch(error => console.error('Error:', error));
-  };
-
   return (
-    <div className="App">
-      <div className="about-header">Edit About Me</div>
-      <img src="https://slatercreekgoldenretrievers.com/wp-content/uploads/2023/05/golden-retriever.jpeg" className="pfp"></img>
-      <form onSubmit={handleSubmit}>
-        <input type="text" onChange={(event) => setName(event.target.value)} placeholder="Name" />
-        <br></br>
-        <textarea onChange={(event) => setAbout(event.target.value)} placeholder="Bio" />
-        <br></br>
-        <button className="submit" type="submit">Submit</button>
-      </form>
-      {users.map(user => (
-        <div key={user._id}>
-          <h2>{user.name}</h2>
-          <p>{user.about}</p>
-        </div>
-      ))}
-    </div>
+    <><Header />
+      <div className="home-wrapper"> 
+        <img src="https://images.unsplash.com/photo-1494947665470-20322015e3a8?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZG9nJTIwd2Fsa2VyfGVufDB8fDB8fHww"
+          className="home-image"></img>
+        <div className="home-intro">Find a dog sitter or become one yourself!</div>
+      </div>
+    </>
   );
 }
 
--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -1,19 +1,28 @@
 import React from 'react'; 
+import "./header.css";
+import { FaSearch, FaHome, FaBriefcase, FaEnvelope, FaUser, FaPlusSquare} from "react-icons/fa";
 
 const Header = () => {
+  /** Might change the links from a to some component */
   return ( 
-    <div>
-        <div>
-          <h1>Name</h1>
-            <div>
-                <input type="text" placeholder='Search'/>
-                <button>Search</button>
-            </div>
-            <div>
-                <h3>Home</h3> 
-                <h3>Create Post</h3>
-            </div>   
-          </div> 
+    <div className="navbar">
+      <h1 className="navbar-name">Name</h1>
+      <div className="navbar-searchbar">
+        <input type="text" placeholder='Search' className="navbar-search-input" />
+        <button className="navbar-search-button"><FaSearch /></button>
+      </div>
+      <div className="navbar-link-wrapper">
+        <a href="" className="navbar-link">
+          <FaHome className="icon" /><br />Home</a>
+        <a href="" className="navbar-link">
+          <FaBriefcase className="icon" /><br />Jobs</a>
+        <a href="" className="navbar-link">
+          <FaEnvelope className="icon" /><br />Messages</a>
+        <a href="" className="navbar-link">
+          <FaUser className="icon"/><br />Profile</a>
+        <a href="" className="navbar-link">
+          <FaPlusSquare className="icon" /><br />Create Post</a>
+      </div>
     </div>
   )
 }
